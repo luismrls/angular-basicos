22df16b62f78f1252f916ad947a501617789fb8b
Luis Morales <dev.luismorales@gmail.com>
2022-05-18 15:12:15 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Iroman', 'Hulk', 'Thor', 'Capitán América'];
+  heroeBorrado: string = '';
+
+  constructor(){
+    this.heroeBorrado = '';
+  }
+  borrarHeroe() {
+
+      this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+}
